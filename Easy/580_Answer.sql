d7a43826db6f40df53da03b47577c36ecd02587f
Chengsheng Deng <55109273+ChengshengDeng@users.noreply.github.com>
2020-08-17 18:49:24 -0400

    Delete 580_Answer.sql

--- Easy/580_Answer.sql
+++
@@ -1,7 +0,0 @@
-SELECT d.dept_name AS dept_name, 
-IFNULL(COUNT(s.student_id),0) AS student_number
-FROM student s 
-RIGHT JOIN department d
-ON s.dept_id = d.dept_id 
-GROUP BY d.dept_id
-ORDER BY student_number DESC,1 ASC;  
