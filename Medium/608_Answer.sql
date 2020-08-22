bc3caab520756fb015167fd4b88f2f1c57b2f8e8
Chengsheng Deng <55109273+ChengshengDeng@users.noreply.github.com>
2020-08-22 02:38:34 -0400

    Add files via upload

---
+++ Medium/608_Answer.sql
@@ -0,0 +1,6 @@
+select id, (
+    case when p_id is null then 'Root'
+         when id not in (
+             select distinct p_id from tree where p_id is not null) then 'Leaf'
+         else 'Inner' end) Type
+from tree
