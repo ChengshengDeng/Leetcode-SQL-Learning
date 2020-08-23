d0ec6106e3b948a33c145865884bc06ed48e2eba
Chengsheng Deng <55109273+ChengshengDeng@users.noreply.github.com>
2020-08-23 15:21:26 -0400

    Add files via upload

---
+++ Medium/1070_Answer.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT s.product_id, 
+s.year AS first_year, 
+s.quantity,
+s.price 
+FROM Sales s
+JOIN Product p 
+ON s.product_id = p.product_id 
+WHERE (s.product_id,s.year) IN (SELECT product_id, MIN(year) FROM Sales GROUP BY product_id)
